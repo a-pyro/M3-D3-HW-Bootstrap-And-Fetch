ab811ad7295bdd68f41f5ab7147b09c9a434308f
ardigermenji <ardi.germenji@gmail.com>
2021-03-10 16:50:44 +0100

    ex 4/5/6

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const shit = '💩',
 
         Starting from the current "base" bootstrap layout, implement the following exercise:
 
-        1) When pressing on Load Images button, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your query
-        2) When pressing on Load Seconday Images, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your secondary query
-        3) When the user clicks on the "VIEW" button inside the Card, open the specified image in a modal view
+        x1) When pressing on Load Images button, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your query
+        x2) When pressing on Load Seconday Images, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your secondary query
+       x 3) When the user clicks on the "VIEW" button inside the Card, open the specified image in a modal view
         4) The Edit button should be replace with a "Hide" button. 
         5) When the hide button is pressed, the whole picture card disappears.
         6) Replace the "9 mins" string in the card template with the ID of the Image
@@ -35,20 +35,26 @@ const shit = '💩',
         API Docs: http://www.splashbase.co/api */
 
 const cards = document.querySelectorAll('.card');
-const viewbtns = document.querySelectorAll('.btn.btn-sm.btn-outline-secondary:first-of-type');
+const viewbtns = document.querySelectorAll(
+  '.btn.btn-sm.btn-outline-secondary:first-of-type'
+);
+const editBtns = document.querySelectorAll(
+  '.btn.btn-sm.btn-outline-secondary:last-of-type'
+);
+
 const loadImgsBtn = document.querySelector('.jumbotron a.btn.btn-primary');
 const loadSecondaryImgsBtn = document.querySelector(
   '.jumbotron a.btn.btn-secondary'
 );
 
 const modal = document.getElementById('exampleModal');
-console.log(modal)
+console.log(modal);
 loadImgsBtn.addEventListener('click', fetchData);
 loadSecondaryImgsBtn.addEventListener('click', fetchData);
-viewbtns.forEach(btn=>{
-  btn.addEventListener('click', showModal)
-  btn.setAttribute('data-toggle', 'modal')
-  btn.setAttribute('data-target', '#exampleModal')
+viewbtns.forEach((btn) => {
+  btn.addEventListener('click', showModal);
+  btn.setAttribute('data-toggle', 'modal');
+  btn.setAttribute('data-target', '#exampleModal');
 });
 
 const endpoint = 'http://www.splashbase.co/api/v1/images/search?query=';
@@ -66,8 +72,10 @@ function fetchData(e) {
     .then((res) => res.json())
     .then((data) => {
       const { images } = data;
-      // console.log(images);
+      console.log(images);
       cards.forEach((card, idx) => {
+        const small = card.querySelector('small');
+        small.innerText = images[idx].id;
         card.firstElementChild.remove(); //get rid of svg
         const imgHTML = `<img class="card-img-top" src="${images[idx].url}" alt="Card image cap"></img>`;
         card.insertAdjacentHTML('afterbegin', imgHTML); //inserting img
@@ -82,4 +90,16 @@ function showModal(e) {
   const url = img.src;
   const modalBody = modal.querySelector('.modal-body');
   modalBody.innerHTML = `<img class="card-img-top" src="${url}" alt="Card image cap"></img>`;
+}
+
+//ex4 /5
+editBtns.forEach((btn) => {
+  btn.innerText = 'Hide';
+  btn.addEventListener('click', hide);
+});
+
+function hide(e) {
+  console.log(e.target);
+  const card = e.target.closest('.card');
+  card.style.opacity = '0';
 }
